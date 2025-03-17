63f73b17aaf5f56b0c95dbd51c5f034af0e66790
anhnguyen12 <anhnguyentl1312@gmail.com>
2025-03-17 16:18:05 +0700

    cors

--- codeigniter/application/config/hooks.php
+++ codeigniter/application/config/hooks.php
@@ -14,7 +14,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $hook['post_controller_constructor'][] = array(
     'class'    => 'Cors',
     'function' => 'enable_cors',
-    'filename' => 'Cors.php',
+    'filename' => 'cors.php',
     'filepath' => 'hooks'
 );
 
+// $hook['post_controller_constructor'][] = array(
+//     'class'    => 'Cors',
+//     'function' => 'enable_cors',
+//     'filename' => 'Cors.php',
+//     'filepath' => 'hooks'
+// );
